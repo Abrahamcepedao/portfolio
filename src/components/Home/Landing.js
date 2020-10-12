a0bfc96a12037483ff2af9b9fe8aa215c24dedde
Abrahamcepe <abrahamgolf@gmail.com>
2020-10-12 15:22:32 -0500

    Styled landing frame.. to be further improved

--- src/components/Home/Landing.js
+++ src/components/Home/Landing.js
@@ -1,11 +1,66 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const useStyles = makeStyles((theme) => ({
   landingContainer: {
     width: "100vw",
     height: "100vh",
-    backgroundColor: "#EEEEFF",
+    backgroundImage: "linear-gradient(to bottom, #EEEEFF, #617C8B)",
+  },
+  topContainer: {
+    width: "90%",
+    maxWidth: "800px",
+    margin: "auto",
+    padding: "20px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    width: "80px",
+    objectFit: "contain",
+  },
+  socialIcons: {
+    flex: "1",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  darkButton: {
+    width: "100px",
+    height: "30px",
+    fontWeight: "bolder",
+    backgroundColor: "#010400",
+    borderRadius: "25px",
+    color: "#EEEEFF",
+    textAlign: "center",
+  },
+  infoContainer: {
+    width: "90%",
+    maxWidth: "600px",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    textAlign: "center",
+    color: "#010400",
+  },
+  avatar: {
+    width: "80%",
+    objectFit: "contain",
+    margin: "auto",
+  },
+  title: {
+    fontSize: "50px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+  },
+  text: {
+    fontSize: "25px",
+    fontWeight: "bolder",
   },
 }));
 
@@ -13,9 +68,32 @@ function Landing({classes}) {
     classes = useStyles();
     return (
       <div className={classes.landingContainer}>
-        <h1>landing</h1>
-        <div className={classes.logo}></div>
-        <img src="./img/Home/Landing/logo.png" alt="AC" />
+        <div className={classes.topContainer}>
+          <img
+            src="./img/Home/Landing/logo.png"
+            alt="AC"
+            className={classes.logo}
+          />
+          <div className={classes.socialIcons}>
+            <InstagramIcon style={{ margin: "10px", color: "#010400", fontSize: "25px" }} />
+            <GitHubIcon style={{ margin: "10px", color: "#010400", fontSize: "25px" }} />
+            <LinkedInIcon style={{ margin: "10px", color: "#010400", fontSize: "25px" }} />
+          </div>
+          <button className={classes.darkButton}>Dark Mode</button>
+        </div>
+        <div className={classes.infoContainer}>
+          <img
+            src="./img/Home/Landing/avatar.png"
+            alt="AV"
+            className={classes.avatar}
+          />
+          <div></div>
+          <h1 className={classes.title}>I am Abraham Cepeda</h1>
+          <h3 className={classes.text}>
+            I am a 20 year old computer programmer studying computer science at
+            Tecnológico de Monterrey while developing my technological skills.
+          </h3>
+        </div>
       </div>
     );
 }
--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import HomeIcon from "@material-ui/icons/Home";
+import SchoolIcon from "@material-ui/icons/School";
+import DesktopMacIcon from "@material-ui/icons/DesktopMac";
+import CodeIcon from "@material-ui/icons/Code";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
 
 const useStyles = makeStyles((theme) => ({
   menu: {
@@ -14,12 +19,17 @@ const useStyles = makeStyles((theme) => ({
     height: "80%",
     maxWidth: "700px",
     margin: "auto",
-    border: "radius",
     backgroundColor: "#EEEEFF",
     borderRadius: "40px",
     boxShadow: "0 5px 10px rgba(25, 25, 25, 0.15), 0 10px 10px rgba(25, 25, 25, 0.12)",
-
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    textAlign: "center"
   },
+  menuItem: {
+      cursor: "pointer"
+  }
 }));
 
 function Menu({ classes }) {
@@ -27,7 +37,26 @@ function Menu({ classes }) {
   return (
     <div className={classes.menu}>
       <div className={classes.menuContainer}>
-
+        <div className={classes.menuItem}>
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className={classes.menuItem}>
+          <SchoolIcon />
+          <p>Home</p>
+        </div>
+        <div className={classes.menuItem}>
+          <DesktopMacIcon />
+          <p>Home</p>
+        </div>
+        <div className={classes.menuItem}>
+          <CodeIcon />
+          <p>Home</p>
+        </div>
+        <div className={classes.menuItem}>
+          <ContactMailIcon />
+          <p>Home</p>
+        </div>
       </div>
     </div>
   );
